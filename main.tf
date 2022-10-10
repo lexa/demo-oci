c839c7e7b13ff5a0eb1651546265494b1397ed09
Aleksei Fedotov <aleksei.fedotov@worldcoin.org>
2022-10-10 21:17:02 +0200

    network

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+variable "region" {
+	default = "eu-center-1"
+}
+
 provider "oci" {
-         region = "eu-center-1"
+  region = "${var.region}"
+}
+
+
+variable "compartment_ocid" {
+	default = "ocid1.compartment.oc1..uniqueid"
+}
+
+resource "oci_core_virtual_network" "ExampleVCN" {
+  cidr_block = "10.1.0.0/16"
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "TFExampleVCN"
+  dns_label = "tfexamplevcn"
+}
+
+resource "oci_core_internet_gateway" "ExampleIG" {
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "TFExampleIG"
+  vcn_id = "${oci_core_virtual_network.ExampleVCN.id}"
 }
